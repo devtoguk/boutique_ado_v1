aa06dad071f467ff0f19ca634b0539b2ddec1d9b
devtoguk <devtoguk@gmail.com>
2021-01-20 12:32:58 +0000

    Tidy code formatting issues

--- bag/views.py
+++ bag/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
 from django.contrib import messages
 
 from products.models import Product
 
 # Create your views here.
+
 
 def view_bag(request):
     """ A view that renders the bag contents page """
